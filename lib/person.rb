cb5221eb6cd25f629cd9e8346a3ebd7ae64c1e7b
Jenny <jenny.kats@aol.com>
2016-12-06 09:24:12 -0500

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,18 @@
 class Person
   #your code here
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, :weight, :handed, :complexion, :t_shirt_size, :wrist_size, :glove_size, :pant_length, :pant_width
+
+# We define our initialize method to take in some unspecified attributes object.
+# Then, we iterate over each key/value pair in the attributes hash. The name of
+# the key becomes the name of a setter method and the value associated with the
+ # key is the name of the value you want to pass to that method.
+
+
+  def initialize(attributes)
+   attributes.each {|key, value| self.send(("#{key}="), value)}
+ end
+
+# The .send method is just another way of calling a method on an object.
+
+
 end
